2ad7db2acc3a6f294a46a7747dd6a55844ed342c
Sebastian Neubauer <sebastian.neubauer@amd.com>
2021-05-14 11:35:57 +0200

    Reduce logging skip message

--- src/lib.rs
+++ src/lib.rs
@@ -1341,7 +1341,10 @@ pub async fn run_tests_parallel<'a>(
     };
 
     loop {
-        if options.max_failures != 0 && fails + crashes >= options.max_failures {
+        if options.max_failures != 0
+            && fails + crashes >= options.max_failures
+            && !pending_jobs.is_empty()
+        {
             warn!(logger, "The number of failures is high, skip remaining jobs";
                 "max_failures" => options.max_failures, "failures" => fails + crashes);
             // Do not start new jobs when we have our max number of failures
